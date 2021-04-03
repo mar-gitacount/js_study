de3a5e397c6b94eef32d886204eaccbb61ee8e4a
市川マサル <ichikawamasaru@ichikawamasarunoMac-mini.local>
2021-04-03 16:47:03 +0900

    phpファイル内の文言変更

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>js_studys</title>
 </head>
 <body>
+<div class="title">html,css,jsの勉強一覧クリックして移動可能</div>
 <?php if($handle = opendir('/Applications/MAMP/htdocs/js_study/')):?>
     <?php while (false !== ($entry = readdir($handle))):?>
         <?php $checkdot = substr($entry, 0,1)?>
